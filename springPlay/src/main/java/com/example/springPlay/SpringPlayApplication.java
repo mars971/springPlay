742cac8ead7cfa783e61cf5bd2528b9ed2a330ba
mtissot <mtissot@LFR040540.corp.capgemini.com>
2020-01-25 17:46:27 +0100

    create spring project

---
+++ springPlay/src/main/java/com/example/springPlay/SpringPlayApplication.java
@@ -0,0 +1,14 @@
+package com.example.springPlay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPlayApplication.class, args);
+		//TODO test creation
+	}
+
+}
---
+++ springPlay/src/test/java/com/example/springPlay/SpringPlayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springPlay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
